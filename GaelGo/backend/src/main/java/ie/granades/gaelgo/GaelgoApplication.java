d20d56888c90b5303f221005f93be8ef3f87d2db
granhades <fernanber96@gmail.com>
2026-02-06 18:03:45 +0000

    Add Spring Boot backend (MVP setup)

---
+++ GaelGo/backend/src/main/java/ie/granades/gaelgo/GaelgoApplication.java
@@ -0,0 +1,13 @@
+package ie.granades.gaelgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaelgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaelgoApplication.class, args);
+	}
+
+}
---
+++ GaelGo/backend/src/test/java/ie/granades/gaelgo/GaelgoApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.granades.gaelgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaelgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
